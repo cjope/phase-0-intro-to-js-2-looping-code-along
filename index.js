090356ad5b829c23ea51fdffc3213fa10d668b79
cjope <craigjope@gmail.com>
2021-05-23 23:17:21 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const name = ["Lisa", "Kaitlin", "Jan"]
+const event = "surprise";
+
+function writeCards(name,event) {
+    let cardArray = [];
+    for (let i = 0; i < name.length; i++) {
+        let message = (`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+        cardArray.push(message);
+    }
+    return cardArray
+}
+
+function countDown(count) {
+    let doomsdayTicker = count
+    while (doomsdayTicker >= 0) {
+        console.log(doomsdayTicker--);
+    }
+}
